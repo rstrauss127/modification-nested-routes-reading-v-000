c5a544ca0eb4471cd263782601d8e0769075c934
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-03 20:20:36 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new]
   end
-  resources :posts, only: [:create, :new]
+  resources :posts
 end
